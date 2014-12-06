84e03cb0a960e6239c552d58eecc0542080a1617
Belinda Zeng <belindazeng@college.harvard.edu>
2014-12-06 02:19:30 -0500

    testing to see if location / messaging is working

--- Fire/ViewController.swift
+++ Fire/ViewController.swift
@@ -8,17 +8,64 @@
 
 import UIKit
 import MessageUI
+import CoreLocation
+
 
 class ViewController: UIViewController, MFMessageComposeViewControllerDelegate {
-    @IBAction func sendMessage(sender: AnyObject) {
-        var messageVC = MFMessageComposeViewController()
+   
+    
+    // location stuff
+    
+    @IBOutlet var label: UILabel!
+    
+    var msg = ""
+    
+    var manager: OneShotLocationManager?
+    
+    func getLocation() -> String {
+        //
+        // request the current location
+        //
+        var msgLoc = ""
+        manager = OneShotLocationManager()
+        manager!.fetchWithCompletion {location, error in
+            
+            // fetch location or an error
+            if let loc = location {
+                msgLoc = loc.description
+                self.label.text = loc.description
+            } else if let err = error {
+                self.label.text = err.localizedDescription
+            }
+            
+            // destroy the object immediately to save memory
+            self.manager = nil
+        }
         
-        messageVC.body = "Please help! I'm currently in a life threatening situation and you're my number one emergency contact.";
-        messageVC.recipients = ["1-408-561-0868", "1-936-250-0347"]
-        messageVC.messageComposeDelegate = self;
-        
-        self.presentViewController(messageVC, animated: false, completion: nil)
+        return msgLoc
     }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        var msgLoc = ""
+        manager = OneShotLocationManager()
+        manager!.fetchWithCompletion {location, error in
+            
+            // fetch location or an error
+            if let loc = location {
+                msgLoc = loc.description
+                self.label.text = loc.description
+            } else if let err = error {
+                self.label.text = err.localizedDescription
+            }
+            
+            // destroy the object immediately to save memory
+            self.manager = nil
+        }
+    }
+    
+    // end location stuff
     
     func messageComposeViewController(controller: MFMessageComposeViewController!, didFinishWithResult result: MessageComposeResult) {
         switch (result.value) {
@@ -36,16 +83,51 @@ class ViewController: UIViewController, MFMessageComposeViewControllerDelegate {
         }
     }
     
+    @IBAction func sendMessage(sender: AnyObject) {
+        var messageVC = MFMessageComposeViewController()
+        var msgLoc = getLocation();
+        
+        messageVC.body = "Please help! I'm currently in a life threatening situation and you're my number one emergency contact. Find me at\(msgLoc)";
+        messageVC.recipients = ["1-408-561-0868", "1-936-250-0347"]
+        messageVC.messageComposeDelegate = self;
+        
+        self.presentViewController(messageVC, animated: false, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        println("this coming here?")
+        println(getLocation())
+        //println(self.string)
         
-        var messageVC = MFMessageComposeViewController()
+        //
+        // request the current location
+        //
+        
+        // location stuff
+        /*manager = OneShotLocationManager()
+        manager!.fetchWithCompletion {location, error in
+            
+            // fetch location or an error
+            if let loc = location {
+                self.string = loc.description
+                println(loc.description)
+                println(self.string)
+                self.label.text = loc.description
+            } else if let err = error {
+                self.label.text = err.localizedDescription
+            }
+            
+            // destroy the object immediately to save memory
+            self.manager = nil
+        }*/
+        /*var messageVC = MFMessageComposeViewController()
         
         messageVC.body = "Enter a message";
         messageVC.recipients = ["1-408-561-0868", "1-936-250-0347"]
         messageVC.messageComposeDelegate = self;
         
-        self.presentViewController(messageVC, animated: false, completion: nil)
+        self.presentViewController(messageVC, animated: false, completion: nil) */
         // Do any additional setup after loading the view, typically from a nib.
     }
     

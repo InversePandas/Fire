9b7ab2a4d928eac4d1b2720527e66f15b46217fc
klkennedy <klksunnygirl@Karens-MacBook-Air-2.local>
2014-12-06 00:13:16 -0500

    now loads the sms first

--- Fire/ViewController.swift
+++ Fire/ViewController.swift
@@ -7,19 +7,52 @@
 //
 
 import UIKit
+import MessageUI
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, MFMessageComposeViewControllerDelegate {
+    @IBAction func sendMessage(sender: AnyObject) {
+        var messageVC = MFMessageComposeViewController()
+        
+        messageVC.body = "Please help! I'm currently in a life threatening situation and you're my number one emergency contact.";
+        messageVC.recipients = ["1-408-561-0868", "1-936-250-0347"]
+        messageVC.messageComposeDelegate = self;
+        
+        self.presentViewController(messageVC, animated: false, completion: nil)
+    }
+    
+    func messageComposeViewController(controller: MFMessageComposeViewController!, didFinishWithResult result: MessageComposeResult) {
+        switch (result.value) {
+        case MessageComposeResultCancelled.value:
+            println("Message was cancelled")
+            self.dismissViewControllerAnimated(true, completion: nil)
+        case MessageComposeResultFailed.value:
+            println("Message failed")
+            self.dismissViewControllerAnimated(true, completion: nil)
+        case MessageComposeResultSent.value:
+            println("Message was sent")
+            self.dismissViewControllerAnimated(true, completion: nil)
+        default:
+            break;
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        var messageVC = MFMessageComposeViewController()
+        
+        messageVC.body = "Enter a message";
+        messageVC.recipients = ["1-408-561-0868", "1-936-250-0347"]
+        messageVC.messageComposeDelegate = self;
+        
+        self.presentViewController(messageVC, animated: false, completion: nil)
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-

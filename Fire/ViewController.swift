3449d77aaf925df0b1519371fa7c4929c030ae2f
klkennedy <klksunnygirl@Karens-MacBook-Air-2.local>
2014-12-05 23:00:47 -0500

    Initial Commit

---
+++ Fire/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fire
+//
+//  Created by Karen Kennedy on 12/5/14.
+//  Copyright (c) 2014 Karen Kennedy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
